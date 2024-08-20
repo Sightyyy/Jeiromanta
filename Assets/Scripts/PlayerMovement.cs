6842d9f49e97a452af0a429d202198c4b0f4829f
Sightyyy <medtr333@gmail.com>
2024-08-21 06:18:21 +0700

    sudah bisa jalan sesuai arah kamera
    camera masih trippy, GPT4o expired trialnya

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Accessibility;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5.0f; // Kecepatan pemain
     public float groundDist;
+    public float sensitivity = 100f; // Sensitivitas gerakan kamera
 
     public LayerMask terrainLayer;
     public Rigidbody rb;
     public SpriteRenderer sr;
     [SerializeField] Transform cameraTransform;
+
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
@@ -20,12 +20,13 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        // Menangani interaksi dengan terrain
         RaycastHit hit;
         Vector3 castPos = transform.position;
         castPos.y += 1;
-        if(Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
+        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
         {
-            if(hit.collider != null)
+            if (hit.collider != null)
             {
                 Vector3 movePos = transform.position;
                 movePos.y = hit.point.y + groundDist;
@@ -33,26 +34,39 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
+        // Input pergerakan pemain
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        Vector3 moveDir = new Vector3(x, 0, y);
-        rb.velocity = moveDir * speed;
-        moveDir = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * moveDir;
-        moveDir.Normalize();
 
-        if(x != 0 && x < 0)
+        // Arah pergerakan pemain mengikuti arah kamera
+        Vector3 moveDir = cameraTransform.right * x + cameraTransform.forward * y;
+        moveDir.y = 0; // Pastikan pemain tidak bergerak di sumbu Y
+        moveDir.Normalize();
+        rb.velocity = moveDir * speed;
+
+        // Flipping sprite berdasarkan arah horizontal
+        if (x != 0 && x < 0)
         {
             sr.flipX = true;
         }
-        else if(x != 0 && x > 0)
+        else if (x != 0 && x > 0)
         {
             sr.flipX = false;
         }
+
+        // Kontrol kamera dengan pembalikan arah horizontal
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+
+        // Membalik arah rotasi horizontal (kiri-kanan)
+        mouseX = -mouseX;
+
+        // Rotasi horizontal pemain mengikuti input mouse yang sudah dibalik
+        transform.Rotate(Vector3.up * mouseX);
     }
 
     private void OnApplicationFocus(bool focus)
     {
-        if(focus)
+        if (focus)
         {
             Cursor.lockState = CursorLockMode.Locked;
         }
